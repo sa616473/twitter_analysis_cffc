03cda1df9a89a7e0183aa74de4c629ebae40cd8b
Saitejas Mopuri <saitejas.mopuri@loadsmart.com>
2023-03-24 20:03:57 -0400

    adding nltk stop words

--- src/app.py
+++ src/app.py
@@ -21,10 +21,10 @@ import os
 
 nltk.download('stopwords')
 
-consumer_key = os.getenv('CONSUMER_KEY')
-consumer_secret = os.getenv('CONSUMER_SECRET')
-access_token = os.getenv('ACCESS_TOKEN')
-access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+consumer_key = st.secrets["CONSUMER_KEY"]
+consumer_secret = st.secrets["CONSUMER_SECRET"]
+access_token = st.secrets["ACCESS_TOKEN"]
+access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"]
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
